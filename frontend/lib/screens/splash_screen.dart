46b015751e4a162570664529121ba89ee56cef29
46815978-eng <46815978@qq.com>
2026-07-04 10:15:35 +0800

    Add frontend/lib/screens/splash_screen.dart

---
+++ frontend/lib/screens/splash_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:woxueshe/providers/auth_provider.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _checkLogin();
+  }
+
+  Future<void> _checkLogin() async {
+    final auth = Provider.of<AuthProvider>(context, listen: false);
+    await auth.checkLoginStatus();
+    if (auth.isLoggedIn) {
+      Navigator.pushReplacementNamed(context, '/home');
+    } else {
+      Navigator.pushReplacementNamed(context, '/login');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: CircularProgressIndicator()),
+    );
+  }
+}
